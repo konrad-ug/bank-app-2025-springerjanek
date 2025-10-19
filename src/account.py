8b3a3d0a139e2cd16ff960f8b225475790637018
springerjanek <springerjan2@gmail.com>
2025-10-19 21:29:13 +0200

    feat5

--- src/account.py
+++ src/account.py
@@ -1,13 +1,38 @@
 class Account:
-    def __init__(self, first_name, last_name, pesel,promo_code=None):
+    def __init__(self, first_name, last_name, pesel, promo_code=None):
         self.first_name = first_name
         self.last_name = last_name
         self.balance = 0
-        if(len(str(pesel)) != 11):
+
+        pesel_str = str(pesel)
+
+        if len(pesel_str) != 11 or not pesel_str.isdigit():
             self.pesel = "Invalid"
         else:
-            self.pesel = pesel
-        if promo_code and len(promo_code.split('_'))==2 and promo_code.split('_')[0] == "PROM":
-            self.balance+=50
-        
-            
+            self.pesel = pesel_str
+
+        if self.pesel != "Invalid":
+            if self.czy_przyznac_promo(self.pesel):
+                if promo_code and len(promo_code.split('_')) == 2 and promo_code.split('_')[0] == "PROM":
+                    self.balance += 50
+
+    def czy_przyznac_promo(self, pesel):
+        s = str(pesel)
+        yy = int(s[0:2])
+        mm = int(s[2:4])
+
+        if 1 <= mm <= 12:
+            century = 1900
+        elif 21 <= mm <= 32:
+            century = 2000
+        elif 41 <= mm <= 52:
+            century = 2100
+        elif 61 <= mm <= 72:
+            century = 2200
+        elif 81 <= mm <= 92:
+            century = 1800
+        else:
+            return False  # niepoprawny miesiąc
+
+        rok_urodzenia = century + yy
+        return rok_urodzenia > 1960
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -11,8 +11,7 @@ class TestAccount:
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.balance == 0
-        assert account.pesel == 12345678910
-        assert isinstance(account.pesel,int)
+        assert account.pesel == '12345678910'
 
     def test_account_creation_with_wrong_pesel(self):
         account = Account("John", "Doe",222)
@@ -26,7 +25,7 @@ class TestAccount:
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.pesel == "Invalid"
-        assert account.balance == 50
+        assert account.balance == 0
 
     def test_account_creation_with_wrong_pesel_and_wrong_promo(self):
         account = Account("John", "Doe",222,"TESTTTT")
@@ -39,5 +38,13 @@ class TestAccount:
         account = Account("John", "Doe",12345678910)
         assert account.first_name == "John"
         assert account.last_name == "Doe"
-        assert account.pesel == 12345678910
+        assert account.pesel == '12345678910'
         assert account.balance == 0
+
+    def test_account_creation_with_promo_before_1960(self):
+        account = Account("Anna", "Kowalska", 44051401458, "PROM_ABC")
+        assert account.balance == 0  # brak bonusu, bo urodzona przed 1960
+
+    def test_account_creation_with_promo_after_1960(self):
+        account = Account("Jan", "Nowak", '02270803628', "PROM_ABC")
+        assert account.balance == 50
