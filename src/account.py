a9c6668448f36be11fb606a9b8be235260eba1af
springerjanek <springerjan2@gmail.com>
2025-10-19 17:27:56 +0200

    feat4

--- src/account.py
+++ src/account.py
@@ -1,5 +1,5 @@
 class Account:
-    def __init__(self, first_name, last_name, pesel):
+    def __init__(self, first_name, last_name, pesel,promo_code=None):
         self.first_name = first_name
         self.last_name = last_name
         self.balance = 0
@@ -7,4 +7,7 @@ class Account:
             self.pesel = "Invalid"
         else:
             self.pesel = pesel
+        if promo_code and len(promo_code.split('_'))==2 and promo_code.split('_')[0] == "PROM":
+            self.balance+=50
+        
             
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -20,3 +20,24 @@ class TestAccount:
         assert account.last_name == "Doe"
         assert account.balance == 0
         assert account.pesel == "Invalid"
+
+    def test_account_creation_with_wrong_pesel_and_promo(self):
+        account = Account("John", "Doe",222,"PROM_XYZ")
+        assert account.first_name == "John"
+        assert account.last_name == "Doe"
+        assert account.pesel == "Invalid"
+        assert account.balance == 50
+
+    def test_account_creation_with_wrong_pesel_and_wrong_promo(self):
+        account = Account("John", "Doe",222,"TESTTTT")
+        assert account.first_name == "John"
+        assert account.last_name == "Doe"
+        assert account.pesel == "Invalid"
+        assert account.balance == 0
+    
+    def test_account_creation_with_no_promo(self):
+        account = Account("John", "Doe",12345678910)
+        assert account.first_name == "John"
+        assert account.last_name == "Doe"
+        assert account.pesel == 12345678910
+        assert account.balance == 0
